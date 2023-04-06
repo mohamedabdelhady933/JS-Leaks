21727cb26d7a2882e2a155a520189f03ebed878c
Mohamed Abdelhady <73122852+mohamedabdelhady933@users.noreply.github.com>
2023-04-06 20:05:55 +0200

    Update JSleaks.sh

--- JSleaks.sh
+++ JSleaks.sh
@@ -9,31 +9,37 @@ YELLOW='\033[1;33m'
 
 
 # Check for text file argument
-if [ $# -ne 1 ]; then
-    printf "\n${YELLOW}[i]  Usage: $0 <file.txt> \n"
+if [ $# -ne 2 ]; then
+    printf "\n${YELLOW}[i]  Usage: $0  <target.com> <file.txt> \n"
     exit 1
 fi
 
 # Store text file argument as variable
-file=$1
+file=$2
 
 # Loop through each line in text file
-while read -r endpoint; do
+if [ -f $file ]; then
+    endpoints=$(cat $file);
     # Extract JavaScript files
-    js_files=$(curl -Ls $endpoint | grep -oE "(http|https)://[a-zA-Z0-9./?=_-]*\.js")
-
+    #js_files=$(curl -Ls $endpoint | grep -oE "(http|https)://[a-zA-Z0-9./?=_-]*\.js")
+    printf "\n\r${WHITE} JS-Leaks Tool \n";
+    printf "\n\r${WHITE} Mohamed Abdelhady \n";
     # Loop through each JavaScript file
-    for js_file in $js_files; do
+    for js_file in $endpoints; do
+
+        if [[ $js_file == *"$1"* ]]; then
+                printf "\n${YELLOW}[i] Start Searching in $js_file \n";
         # Check for sensitive information
-        leaks=$(curl -Ls $js_file | grep -oEi "(password|token|api|hidden|secret|apikey|key|credentials|admin|MySQL|aws|OTP|oauth|config|DATABASE|\.bak|\.sql|\.old|\.txt|\.cnf|\.ini|\.save|\.swp|bucket|db_|firebase|@yopmail|@coffeetimer24|@chotunai|internal|backup|\.env|\.json|\.git|id_dsa|\.yaml|\.cfg)" | awk '{print "\n [+] The Word is : " $0}')
+            leaks=$(curl -Ls $js_file | grep -oEi "(sanity|password|token|api|hidden|secret|apikey|key|credentials|admin|MySQL|aws|OTP|oauth|config|DATABASE|\.bak|\.sql|\.old|\.txt|\.cnf|\.ini|\.save|\.swp|bucket|db_|firebase|@yopmail|@coffeetimer24|@chotunai|internal|backup|\.env|\.json|\.git|id_dsa|\.yaml|\.cfg)" | awk '{print "\n [+] The Word is : " $0}')
 
         # Output results
-        if [ -n "$leaks" ]; then
-            printf "\n${WHITE} [+] This Endpoint $js_file has The following match \n ";
-            printf "${GREEN}$leaks" | sort -u ;
-            printf "\n";
-        else
-            printf "\n${RED} [-] No Result Found! \n";
-        fi
+            if [ -n "$leaks" ]; then
+                printf "\n${WHITE} [+] This Endpoint $js_file has The following match \n ";
+                printf "${GREEN}$leaks" | sort -u ;
+                printf "\n";
+            else
+                printf "\n${RED} [-] No Result Found! \n";
+            fi
+       fi
     done
-done < "$file"
+fi
